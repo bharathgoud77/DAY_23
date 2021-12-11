a7f6b52a3c9bf35b7247791deac641e77cb5bcbe
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-12-11 15:36:52 +0530

    List

---
+++ List/List/List.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class List
+    {
+        public static void ListDemo()
+        {
+            List<int> List = new List<int>();
+            List.Add(1);
+            List.Add(2);
+            List.Add(3);
+            List.Add(4);
+           
+            Console.WriteLine("list contains element  ", List);
+             foreach (int obj in List)
+            {
+                Console.WriteLine( obj);
+            }
+        }
+        
+        }
+    }
